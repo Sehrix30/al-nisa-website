1bccf8ba6e1c20c92d4e20bf6bed37263075b028
Sehrish Iftikhar <sehrix30@gmail.com>
2026-09-08 01:41:36 +0500

    Build Get involved page and mobile navigation

--- script.js
+++ script.js
@@ -39,3 +39,13 @@ lightbox.addEventListener('click', function(event) {
     lightbox.classList.remove('active');
   }
 });
+var menuToggle = document.getElementById('menuToggle');
+var navLinks = document.getElementById('nav-Links');
+
+menuToggle.addEventListener('click', function() {
+    navLinks.classList.toggle('is-open');
+
+    var isOpen = navLinks.classList.contains('is-open');
+
+    menuToggle.setAttribute('aria-expanded', isOpen);
+});
